7402cf63b3bbf249a9953f029f708a15d3cf4450
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2020-10-06 11:39:17 +0900

    comasy init

---
+++ Comasy/app/src/main/java/com/example/comasy/MemberRegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.comasy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MemberRegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_member_registration)
+    }
+}
