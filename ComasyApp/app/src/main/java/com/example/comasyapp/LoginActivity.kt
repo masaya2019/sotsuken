712dcad61847ec8331ae6e0b1a4cbee99d717b78
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2020-10-14 21:41:58 +0900

    ログイン認証を追加

--- ComasyApp/app/src/main/java/com/example/comasyapp/LoginActivity.kt
+++ ComasyApp/app/src/main/java/com/example/comasyapp/LoginActivity.kt
@@ -3,7 +3,13 @@ package com.example.comasyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,18 +26,77 @@ class LoginActivity : AppCompatActivity() {
         // ログインボタン（transitionHomeActivityButton）をクリックしたら、
         transitionHomeActivityButton.setOnClickListener {
 
-//            // 入力されたメールアドレスを取得
-//            var mailAddress = inputEmailAddress.text
-//
-//            // 入力されたパスワードを取得
-//            var inputPassword = inputPassword.text
+            // 入力されたメールアドレスを取得
+            val mailAddress = inputEmailAddress.text.toString()
+
+            // 入力されたパスワードを取得
+            val inputPassword = inputPassword.text.toString()
+
+            val handler = Handler()
 
             // メールアドレスとパスワードが一致するかを返すＡＰＩにリクエストを送る
-
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+            LoginCheck(mailAddress, inputPassword, handler, errortextLogin);
 
         }
 
+    }
+
+    // メールアドレスとパスワードが一致するかを返すＡＰＩにリクエストを送る
+    private fun LoginCheck(mail_address: String, password: String, handler: Handler, errorText: TextView) {
+
+        val url = "http://10.0.2.2/sotsuken/api/login_check.php"
+
+        val body = FormBody.Builder(charset("UTF-8"))
+            .add("mail_address", mail_address)
+            .add("password", password)
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(body)
+            .build()
+
+        OkHttpClient().newCall(request).enqueue(object : Callback {
+
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) {
+
+                // responseのstatusに対応する値（）を取得
+                val jsonData = JSONObject(response.body()?.string())
+                val apiStatus = jsonData.getString("status")
+
+                Log.i("apiStatus", apiStatus)
+
+                // responseのstatusによって次の画面に進むorエラーを表示する
+                when (apiStatus) {
+
+                    //  データベースに登録された場合
+                    "yes" -> {
+                        // エラーを表示
+                        handler.post {
+                            errorText.text = "ログインします！"
+                        }
+                        // ホーム画面（HomeActivity.kt）へ遷移する
+                        val intent = Intent(applicationContext, HomeActivity::class.java)
+                        startActivity(intent)
+                    }
+//                    // 以下はエラー用に仮作成
+//                    // えらー１
+//                    "" -> {
+//                        // エラーを表示
+//                        handler.post {
+//                            errorText.text = ""
+//                        }
+//                    }
+//                    // えらー２
+//                    "" -> {
+//                        // エラーを表示
+//                        handler.post {
+//                            errorText.text = ""
+//                        }
+//                    }
+                }
+            }
+        })
     }
 }
