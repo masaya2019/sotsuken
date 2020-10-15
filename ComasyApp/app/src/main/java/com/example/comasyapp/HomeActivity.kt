3bcc95b613588e6d26dca49ce2553dc873fac717
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2020-10-15 09:47:24 +0900

    メニューバーの処理を追加

--- ComasyApp/app/src/main/java/com/example/comasyapp/HomeActivity.kt
+++ ComasyApp/app/src/main/java/com/example/comasyapp/HomeActivity.kt
@@ -40,6 +40,36 @@ class HomeActivity : AppCompatActivity() {
 
 
 
-
+        // メニューバーをクリックしたときの処理
+        transitionRefrigeratorButton.setOnClickListener {
+            // Home画面(HomeActivity.kt)へ遷移
+            val intent = Intent(this, HomeActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+        transitionColumnButton.setOnClickListener {
+            // コラム画面(ColumnActivity.kt)へ遷移
+            val intent = Intent(this, ColumnActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+        transitionSearchButton.setOnClickListener {
+            // レシピ検索画面(RecipeSearchActivity.kt)へ遷移
+            val intent = Intent(this, RecipeSearchActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+        transitionMemoButton.setOnClickListener {
+            // メモ画面(MemoActivity.kt)へ遷移
+            val intent = Intent(this, MemoActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+        transitionSettingButton.setOnClickListener {
+            // 設定画面(SettingActivity.kt)へ遷移
+            val intent = Intent(this, SettingActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
         }
     }
+}
