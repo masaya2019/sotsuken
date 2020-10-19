63b4bd6ca936ba8eed81fef00fe30cbe27933917
n1290504 <bjrf1290504@gn.iwasaki.ac.jp>
2020-10-19 11:56:07 +0900

    登録画面のレイアウト作成

---
+++ ComasyApp/app/src/main/java/com/example/comasyapp/RegistrationActivity.kt
@@ -0,0 +1,54 @@
+package com.example.comasyapp
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+        // 本体からメールアドレスとパスワードを取得
+        // https://maku77.github.io/android/fw/shared-preference.html
+        val pref = getSharedPreferences("my_password", Context.MODE_PRIVATE)
+        val login_mail_address = pref.getString("mail_address", "")
+        val login_password = pref.getString("password", "")
+
+
+
+        // メニューバーをクリックしたときの処理
+        transitionRefrigeratorButton.setOnClickListener {
+            // Home画面(HomeActivity.kt)へ遷移
+            val intent = Intent(this, HomeActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+        transitionColumnButton.setOnClickListener {
+            // コラム画面(ColumnActivity.kt)へ遷移
+            val intent = Intent(this, ColumnActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+        transitionSearchButton.setOnClickListener {
+            // レシピ検索画面(RecipeSearchActivity.kt)へ遷移
+            val intent = Intent(this, RecipeSearchActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+        transitionMemoButton.setOnClickListener {
+            // メモ画面(MemoActivity.kt)へ遷移
+            val intent = Intent(this, MemoActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+        transitionSettingButton.setOnClickListener {
+            // 設定画面(SettingActivity.kt)へ遷移
+            val intent = Intent(this, SettingActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+    }
+}
