642a21e72a237f534ac38719d836f6bf947c3567
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2020-10-21 14:08:09 +0900

    処理をRegistrationClickActivityに移動

--- ComasyApp/app/src/main/java/com/example/comasyapp/RegistrationClickActivity.kt
+++ ComasyApp/app/src/main/java/com/example/comasyapp/RegistrationClickActivity.kt
@@ -3,6 +3,8 @@ package com.example.comasyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_home.cat01btn
 import kotlinx.android.synthetic.main.activity_home.transitionColumnButton
@@ -10,11 +12,83 @@ import kotlinx.android.synthetic.main.activity_home.transitionMemoButton
 import kotlinx.android.synthetic.main.activity_home.transitionRefrigeratorButton
 import kotlinx.android.synthetic.main.activity_home.transitionSearchButton
 import kotlinx.android.synthetic.main.activity_home.transitionSettingButton
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class RegistrationClickActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_click)
+
+        // 最初に冷蔵庫の中身データを受け取りたい！
+        val handler = Handler()
+
+//        val url = "http://10.0.2.2/sotsuken/api/testdata.json"
+        val url = "http://10.0.2.2/sotsuken/api/response_all_goods.php"
+
+        val body = FormBody.Builder(charset("UTF-8"))
+            // 仮のcategory_id
+            .add("category_id", "cat01")
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(body)
+            .build()
+
+        OkHttpClient().newCall(request).enqueue(object : Callback {
+
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) {
+
+                // 全体のJSONObjectをとる
+                val jsonObj = JSONObject(response.body()?.string())
+                // responseのstatusに対応する値（）を取得
+                val apiStatus = jsonObj.getString("status")
+
+                Log.i("ホーム画面のapiStatus", apiStatus)
+
+                // responseのstatusによって次の画面に進むorエラーを表示する
+                when (apiStatus) {
+
+                    //  データベースに登録された場合
+                    "yes" -> {
+
+                        val datas = jsonObj.getJSONArray("data")
+
+                        for (i in 0 until datas.length()) {
+
+                            val zeroJsonObj = datas.getJSONObject(i)
+
+                            //
+                            val goods_id = zeroJsonObj.getString("goods_id")
+
+                            Log.i("ホームのgoods_id", goods_id)
+
+                            //
+                            val goods_name = zeroJsonObj.getString("goods_name")
+
+                            Log.i("ホームのgoods_name", goods_name)
+
+                            //
+                            val goods_picture_name = zeroJsonObj.getString("goods_picture_name")
+
+                            Log.i("ホームのgoods_picture_name", goods_picture_name)
+
+                            // テーブル作成
+                            createTableView(i)
+                        }
+
+                        // エラーを表示
+                        handler.post {
+                            //
+                        }
+                    }
+
+                }
+            }
+        })
 
         // category（野菜）ボタンをクリックしたら
         cat01btn.setOnClickListener {
@@ -62,5 +136,15 @@ class RegistrationClickActivity : AppCompatActivity() {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.container, fragment)
         fragmentTransaction.commit()
+    }
+
+    // テーブルを生成
+    fun createTableView(i : Int) {
+        var row = i / 4
+        var col = i % 4
+
+        Log.i("テーブルrow", row.toString())
+        Log.i("テーブルcol", col.toString())
+
     }
 }
