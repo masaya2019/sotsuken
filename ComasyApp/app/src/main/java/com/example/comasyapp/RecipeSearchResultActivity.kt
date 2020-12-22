9a4ab5ca84a4e9d9313dc3d1f7cc35dd213445a9
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2020-12-22 11:44:31 +0900

    レシピ検索画面（仮データ読み込み）

--- ComasyApp/app/src/main/java/com/example/comasyapp/RecipeSearchResultActivity.kt
+++ ComasyApp/app/src/main/java/com/example/comasyapp/RecipeSearchResultActivity.kt
@@ -4,10 +4,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.MotionEvent
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_home.transitionColumnButton
 import kotlinx.android.synthetic.main.activity_home.transitionMemoButton
@@ -15,8 +21,11 @@ import kotlinx.android.synthetic.main.activity_home.transitionRefrigeratorButton
 import kotlinx.android.synthetic.main.activity_home.transitionSearchButton
 import kotlinx.android.synthetic.main.activity_home.transitionSettingButton
 import kotlinx.android.synthetic.main.activity_registration_click.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
-class RecipeSearchActivity : AppCompatActivity() {
+class RecipeSearchResultActivity : AppCompatActivity() {
 
     private lateinit var background: ConstraintLayout
 
@@ -25,13 +34,16 @@ class RecipeSearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recipe_search)
+        setContentView(R.layout.activity_recipe_search_result)
 
         // 背景のレイアウトを取得
         background = findViewById(R.id.background)
 
         // InputMethodManagerを取得
         inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        // レシピタイトルを表示
+        makeRecyclerData()
 
         // メニューバーをクリックしたときの処理
         transitionRefrigeratorButton.setOnClickListener {
@@ -70,24 +82,76 @@ class RecipeSearchActivity : AppCompatActivity() {
         // ==============================
         searchImageView.setOnClickListener {
 
-            // 仮遷移
-            val intent = Intent(this, RecipeSearchResultActivity::class.java)
-                .setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
+            // 入力された検索キーワードを取得
+            val inputSearchKeyword = inputSearchText.text.toString()
 
-//            // 入力された検索キーワードを取得
-//            val inputSearchKeyword = inputSearchText.text.toString()
-//
-//            // 入力された値が空でなければ
-//            if (inputSearchKeyword != "") {
-//                // 食材、キーワードをDBから検索する
-//            }
+            // 入力された値が空でなければ
+            if (inputSearchKeyword != "") {
+                // 食材、キーワードをDBから検索する
+            }
 
             // キーボードを隠す
             inputMethodManager.hideSoftInputFromWindow(searchImageView.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS)
             // 背景にフォーカスを移す
             searchImageView.requestFocus()
         }
+    }
+
+    // ================
+    // レシピタイトルを表示
+    // ================
+    fun makeRecyclerData() {
+
+        val handler = Handler()
+
+        // リクエスト先URL
+        val url = "http://r02isc2t119.sub.jp/api/test.json"
+
+        val body = FormBody.Builder(charset("UTF-8"))
+//            .add("refrigerator_id", now_refrigerator_id)
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(body)
+            .build()
+
+        OkHttpClient().newCall(request).enqueue(object : Callback {
+
+            // リクエスト結果受取に失敗
+            override fun onFailure(call: Call, e: IOException) {}
+
+            // リクエスト結果受取に成功
+            override fun onResponse(call: Call, response: Response) {
+
+                // 全体のJSONObjectをとる
+                val jsonObj = JSONObject(response.body()?.string())
+
+                //　dataをjson配列に入れる
+                val datas = jsonObj.getJSONArray("result")
+
+                handler.post {
+                    // データを1個づつ取り出す
+                    for (i in 0 until datas.length()) {
+
+                        // 1レコードをjsonObjectに入れる
+                        val zeroJsonObj = datas.getJSONObject(i)
+
+                        // レシピ画像を取得
+                        val foodImageUrl = zeroJsonObj.getString("foodImageUrl")
+                        // レシピ名を取得
+                        val recipeTitle = zeroJsonObj.getString("recipeTitle")
+                        // レシピURLを取得
+                        val recipeUrl = zeroJsonObj.getString("recipeUrl")
+
+                        Log.i(
+                            "取得レコード",
+                            "${foodImageUrl} ${recipeTitle} ${recipeUrl}"
+                        )
+                    }
+                }
+            }
+        })
     }
 
     // 画面タップ時に呼ばれる
